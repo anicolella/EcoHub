2074f4f444f44f35c31496427400938a990329e0
shiro <jodomingues@usp.br>
2025-09-28 20:22:35 -0300

    Talvez tenha ocoorrido alguns erros na parte de particionamento.

--- _SCRIPTS_BD_AMBIENTAL/R/Codigo preliminar Joao/filetinho.R
+++ _SCRIPTS_BD_AMBIENTAL/R/Codigo preliminar Joao/filetinho.R
@@ -4,4 +4,4 @@ df_novo <- separate_rows(MTTEMPORAL, origem, sep = ", ")
 df_novo2 <- separate_rows(BRASILTEMPORAL, origem, sep = ", ")
 df_novo2 <- separate_rows(df_novo2, origem, sep = " e ")
 df_novo2 <- separate_rows(df_novo2, origem, sep = ",")
-
+df_novo2 <- separate_rows(df_novo2, origem, sep = ";")
