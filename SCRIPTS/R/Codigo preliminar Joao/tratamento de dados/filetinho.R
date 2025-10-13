144ef8d4e8cac79b380df2a960050fc3ae99c6d4
shiro <jodomingues@usp.br>
2025-10-13 13:51:49 -0300

    filtragem de dados, melhora na separação.

--- SCRIPTS/R/Codigo preliminar Joao/tratamento de dados/filetinho.R
+++ SCRIPTS/R/Codigo preliminar Joao/tratamento de dados/filetinho.R
@@ -7,6 +7,7 @@ df_novo2 <- separate_rows(BRASILTEMPORAL, origem, sep = ", ")
 df_novo2 <- separate_rows(df_novo2, origem, sep = " e ")
 df_novo2 <- separate_rows(df_novo2, origem, sep = ",")
 df_novo2 <- separate_rows(df_novo2, origem, sep = ";")
+df_novo2 <- separate_rows(df_novo2, origem, sep = ":")
 df_novo2 <- separate_rows(df_novo2, origem, sep = "/")
 df_novo2 <- df_novo2 %>%
   mutate(origem = str_replace_all(origem, "\\.", ""))
