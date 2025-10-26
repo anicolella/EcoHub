b3d4c371fc6a8a3043910345ce0213e044fad1e0
shiro <jodomingues@usp.br>
2025-10-26 19:02:51 -0300

    .

--- SCRIPTS/R/Codigo preliminar Joao/Graficos/matogrosso.R
+++ SCRIPTS/R/Codigo preliminar Joao/Graficos/matogrosso.R
@@ -1,12 +1,24 @@
 library(geobr)
 library(tidyverse)
 
+resultado_igpm_espaço <- resultado_igpm %>%
+  mutate(
+    esp = ifelse(ano == 2023, 2022, ano)
+  )
+
+mteste <- resultado_igpm |> filter(UF.x == "MT") 
+
 lista_de_argumentos <- list(
-  code_muni = resultado_igpm$id_municipio,
-  year = resultado_igpm$ano
+  code_muni = mteste$id_municipio,
+  esp = mteste$ano
 )
+
+
+
 
 malha_final <- pmap_dfr(
   lista_de_argumentos,
   read_municipality
 )
+
+geobr::list_geobr()
