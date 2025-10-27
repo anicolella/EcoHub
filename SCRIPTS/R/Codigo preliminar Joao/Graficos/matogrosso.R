b9d5266b14ff4239fe52627021570b9c3113208e
shiro <jodomingues@usp.br>
2025-10-27 10:15:27 -0300

    teste para o ano de 2019, estado do Mato Grosso, deu certo! :D

--- SCRIPTS/R/Codigo preliminar Joao/Graficos/matogrosso.R
+++ SCRIPTS/R/Codigo preliminar Joao/Graficos/matogrosso.R
@@ -1,13 +1,44 @@
 library(geobr)
 library(tidyverse)
 
+library(sf)
+
+# Caminho completo do arquivo shapefile
+muni <- st_read("C:/Users/jodom/Downloads/br_municipios_20200807/BR_Municipios_2019.shp")
+
 
 mteste <- resultado_igpm |> filter(UF.x == "MT") |> filter(ano == 2019)
 
-lista_de_argumentos <- list(
-  code_muni = mteste$id_municipio,
-  year = mteste$ano  # CORRIGIDO
-)
+mteste <- left_join(mteste, muni, by = c("id_municipio" = "CD_MUN"))
+
+library(sf) # Certifique-se de que 'sf' esteja carregado
+
+texting <- mteste |> 
+  filter(UF.x == "MT") |> 
+  filter(ano == 2019) |> 
+  filter(nivel == 1 & tipologia_de_uso == "MÉDIA GERAL")
+
+mteste2 <- mteste |> 
+  filter(UF.x == "MT") |> 
+  filter(ano == 2019) |> 
+  filter(nivel == 0 & tipologia_de_uso == "MÉDIA GERAL") |> 
+  
+  # --- AQUI ESTÁ A CORREÇÃO ---
+  # Transforme seus dados em um objeto 'sf' antes de plotar
+  # (Troque "geometry" se o nome da sua coluna for outro)
+  st_as_sf(sf_column_name = "geometry") |> 
+  
+  ggplot() +
+  # Agora o geom_sf() vai achar a geometria automaticamente
+  geom_sf(aes(fill = c_vti_media), color = "gray40", linewidth = 0.2) +
+  
+  scale_fill_distiller(palette = "YlOrRd", direction = 1) +
+  labs(
+    title = "Valor da terra com imóvel - Mato Grosso",
+    fill = "R$/ha"
+  ) +
+  theme_minimal()
+print(mteste2)
 
 malha_final <- pmap_dfr(
   lista_de_argumentos,
