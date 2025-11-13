6dbc2f5885af79bd3b8ebb7919d740f96981c7b2
shiro <jodomingues@usp.br>
2025-11-13 09:35:12 -0300

    grafico bar medias

---
+++ SCRIPTS/R/Codigo preliminar Joao/Graficos/terras_uso.R
@@ -0,0 +1,31 @@
+library(tidyverse)
+library(geobr)
+library(ipeadatar)
+
+read.csv()
+
+read_typ <- d |> 
+  filter(nivel == 1) |> 
+ # pull(tipologia_de_uso) |> 
+  unique() |> distinct(tipologia_de_uso)
+
+pecuária <- d |> filter(tipologia_de_uso )
+
+estado_media <- df_limpo |>
+  # 1. Agrupe pelas UFs (sem o df_limpo$)
+  group_by(SIGLA_UF) |> 
+  
+  # 2. Use summarise() para "resumir" cada grupo em um único valor
+  #    Aqui nós damos um nome para a nova coluna (ex: gini_uf)
+  summarise(
+    media_por_estado = mean(uc_vti_media)
+  )
+
+medias <- estado_media |>
+  ggplot(aes(x = SIGLA_UF, y = media_por_estado)) +  # Define x and y axes here
+  geom_col()                         # geom_col() is for pre-calculated values
+
+print(medias)
+
+as.numeric()
+mediassp <- mean(SPTEMPORAL, vti_media, na.rm = TRUE)
