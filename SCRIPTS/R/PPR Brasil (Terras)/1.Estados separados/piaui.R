d7890a38ede6393a74e060ebec1a86ebf520d01e
shiro <jodomingues@usp.br>
2025-11-22 15:52:26 -0300

    att piaui

--- SCRIPTS/R/PPR Brasil (Terras)/1.Estados separados/piaui.R
+++ SCRIPTS/R/PPR Brasil (Terras)/1.Estados separados/piaui.R
@@ -325,6 +325,510 @@ MRT12_2022 <- data.frame(
   origem = "Bom Princípio do Piauí, Buriti dos Lopes, Cajueiro da Praia, Cocal, Cocal dos Alves, Ilha Grande, Luís Correia e Parnaíba."
 )
 
+### Atlas 2023 #####
+
+
+MRT2401_2024 <- data.frame(
+  mrt = "MERCADO REGIONAL DE TERRAS SERRA DO QUILOMBO (MRT-2401)",
+  tipologia_de_uso = c(
+    "Geral",
+    "Agrícola",
+    "Exploração Mista",
+    "Pecuária",
+    "Vegetação Nativa",
+    "Agrícola - Grãos Diversos",
+    "Exploração Mista",
+    "Pecuária diversos",
+    "Vegetação Nativa - Cerrado"
+  ),
+  nivel = c(0, 1, 1, 1, 1, 2, 2, 2, 2),
+  vti_media = c(
+    1471.75, 6909.28, 1125.84, 1257.77, 1083.88, 6909.28, 1125.84, 1257.77,
+    1083.88
+  ),
+  vti_minimo = c(
+    340.44, 578.82, 714.25, 749.75, 340.44, 578.82, 714.25, 749.75, 340.44
+  ),
+  vti_maximo = c(
+    19480.98, 19480.98, 2322.00, 2191.96, 2430.00, 19480.98, 2322.00, 2191.96,
+    2430.00
+  ),
+  vtn_media = c(
+    1396.75, 6284.23, 1034.83, 1257.77, 1083.88, 6284.23, 1034.83, 1257.77,
+    1083.88
+  ),
+  vtn_minimo = c(
+    340.44, 578.82, 714.25, 749.75, 340.44, 578.82, 714.25, 749.75, 340.44
+  ),
+  vtn_maximo = c(
+    18413.77, 18413.77, 1496.66, 2191.96, 2430.00, 18413.77, 1496.66, 2191.96,
+    2430.00
+  ),
+  origem = "Antônio Almeida, Baixa Grande do Ribeiro, Barreiras do Piauí, Bertolínia, Currais,
+Gilbués, Manoel Emídio, Monte Alegre do Piauí, Palmeira do Piauí, Ribeiro Gonçalves,
+Santa Filomena, São Gonçalo do Gurguéia, Sebastião Leal e Uruçuí"
+)
+
+MRT2402_2024 <- data.frame(
+  mrt = "MERCADO REGIONAL DE TERRAS VALE DO GURGUÉIA (MRT-2402)",
+  tipologia_de_uso = c(
+    "Geral",
+    "Agrícola",
+    "Exploração Mista",
+    "Pecuária",
+    "Vegetação Nativa",
+    "Agrícola - Grãos Diversos",
+    "Exploração Mista - Agrícola + Pecuária",
+    "Exploração Mista - Diversificada",
+    "Pecuária - Bovino - Pastagem Formada",
+    "Pecuária bovino pastagem nativa",
+    "Vegetação Nativa caatinga",
+    "Vegetação Nativa cerrado"
+  ),
+  nivel = c(0, 1, 1, 1, 1, 2, 2, 2, 2, 2, 2, 2),
+  vti_media = c(
+    1206.35, 18471.03, 1223.36, 1664.18, 989.62, 18471.03, 1217.05, 2695.66,
+    1592.55, 2070.09, 672.62, 1068.87
+  ),
+  vti_minimo = c(
+    240.00, 9777.81, 240.00, 662.92, 305.86, 9777.81, 1100.00, 700.00, 662.92,
+    1207.74, 305.86, 500.00
+  ),
+  vti_maximo = c(
+    26514.80, 26514.80, 2413.79, 3200.00, 1842.07, 26514.80, 1351.35, 5678.54,
+    3200.00, 2750.00, 1206.90, 1842.07
+  ),
+  vtn_media = c(
+    1049.77, 17168.52, 1084.79, 1332.08, 921.22, 17168.52, 1217.05, 1754.70,
+    1279.66, 1629.09, 672.62, 983.37
+  ),
+  vtn_minimo = c(
+    189.87, 9777.81, 240.00, 380.58, 189.87, 9777.81, 1100.00, 698.64, 380.58,
+    553.89, 305.86, 189.87
+  ),
+  vtn_maximo = c(
+    24176.09, 24176.09, 1873.35, 2876.35, 1842.07, 24176.09, 1274.67, 3004.56,
+    2876.35, 2252.54, 1206.90, 1842.07
+  ),
+  origem = "Alvorada do Gurguéia, Avelino Lopes, Bom Jesus, Colônia do Gurguéia, Corrente,
+Cristalândia do Piauí, Cristino Castro, Curimatá, Júlio Borges, Morro Cabeça no Tempo,
+Parnaguá, Redenção do Guruguéia, Riacho Frio, Santa Luz e Sebastião Barro"
+)
+
+MRT2403_2024 <- data.frame(
+  mrt = "MERCADO REGIONAL DE TERRAS NORTE PIAUIENSE (MRT-2403)",
+  tipologia_de_uso = c(
+    "Geral",
+    "Agrícola",
+    "Exploração Mista",
+    "Pecuária",
+    "Agrícola - Grãos Diversos",
+    "Exploração Mista - Agrícola + Pecuária",
+    "Exploração Mista - Diversificada",
+    "Pecuária - Bovino - Pastagem Formada"
+  ),
+  nivel = c(0, 1, 1, 1, 2, 2, 2, 2),
+  vti_media = c(
+    962.78, 925.28, 1028.38, 1027.36, 925.28, 1146.62, 1026.38, 1027.36
+  ),
+  vti_minimo = c(
+    136.87, 884.59, 260.93, 136.87, 884.59, 993.11, 260.93, 136.87
+  ),
+  vti_maximo = c(
+    2407.32, 969.36, 2407.32, 1912.65, 969.36, 1308.12, 2407.32, 1912.65
+  ),
+  vtn_media = c(
+    894.87, 925.28, 911.83, 964.83, 925.28, 1146.62, 889.02, 964.83
+  ),
+  vtn_minimo = c(
+    79.84, 884.59, 260.93, 79.84, 884.59, 993.11, 260.93, 79.84
+  ),
+  vtn_maximo = c(
+    2007.21, 969.36, 2007.21, 1912.65, 969.36, 1308.12, 1999.85, 1912.65
+  ),
+  origem = "Campo Largo do Piauí, Caraúbas do Piauí, Caxingó, Joaquim Pires, Joca Marques,
+Luzilândia, Madeiro, Matias Olímpio, Miguel Alves, Murici dos Portelas, Nossa Senhora
+dos Remédios, Porto e São João do Arraia"
+)
+
+MRT2404_2024 <- data.frame(
+  mrt = "MERCADO REGIONAL DE TERRAS SERRA DA CAPIVARA (MRT-2404)",
+  tipologia_de_uso = c(
+    "Geral",
+    "Agrícola",
+    "Exploração Mista",
+    "Pecuária",
+    "Agrícola - Grãos Diversos",
+    "Exploração Mista - Diversificada",
+    "Pecuária - Diversos"
+  ),
+  nivel = c(0, 1, 1, 1, 2, 2, 2),
+  vti_media = c(
+    510.93, 400.60, 502.49, 663.27, 400.60, 502.49, 663.27
+  ),
+  vti_minimo = c(
+    112.25, 185.00, 112.25, 170.00, 185.00, 112.25, 170.00
+  ),
+  vti_maximo = c(
+    1427.96, 633.33, 1427.96, 1100.00, 633.33, 1427.96, 1100.00
+  ),
+  vtn_media = c(
+    412.96, 400.60, 392.00, 571.24, 400.60, 392.00, 571.24
+  ),
+  vtn_minimo = c(
+    112.25, 185.00, 112.25, 170.00, 185.00, 112.25, 170.00
+  ),
+  vtn_maximo = c(
+    1096.56, 633.33, 1000.00, 1096.56, 633.33, 1000.00, 1096.56
+  ),
+  origem = "Anísio de Abreu, Bonfim do Piauí, Brejo do Piauí, Canto do Buriti, Caracol, Coronel José
+Dias, Dirceu Arcoverde, Dom Inocêncio, Eliseu Martins, Fartura do Piauí, Flores do
+Piauí, Guaribas, Jurema, Pajeú do Piauí, Pavussu, Rio Grande do Piauí, São Braz do
+Piauí, São Lourenço do Piauí, São Raimundo Nonato, Tamboril do Piauí e Várzea Branca"
+)
+
+MRT2405_2024 <- data.frame(
+  mrt = "MERCADO REGIONAL DE TERRAS VALE DO RIO PIAUI (MRT-2405)",
+  tipologia_de_uso = c(
+    "Geral",
+    "Agrícola",
+    "Exploração Mista",
+    "Pecuária",
+    "Vegetação Nativa",
+    "Agrícola - Diversificada",
+    "Exploração Mista - Diversificada",
+    "Pecuária - Bovino - Pastagem Formada",
+    "Pecuária - Diversos",
+    "Vegetação Nativa - Cerrado"
+  ),
+  nivel = c(0, 1, 1, 1, 1, 2, 2, 2, 2, 2),
+  vti_media = c(
+    591.06, 1384.52, 596.08, 543.79, 877.36, 1384.52, 642.57, 566.34, 244.37, 877.36
+  ),
+  vti_minimo = c(
+    100.21, 1182.78, 160.00, 100.21, 571.10, 1182.78, 160.00, 250.00, 202.59, 571.10
+  ),
+  vti_maximo = c(
+    1500.19, 1500.19, 1178.57, 1200.00, 1441.14, 1500.19, 1359.94, 1000.00, 266.61, 1441.14
+  ),
+  vtn_media = c(
+    477.08, 1267.15, 412.97, 470.46, 877.36, 1267.15, 438.81, 495.57, 177.15, 877.36
+  ),
+  vtn_minimo = c(
+    100.21, 1139.58, 119.27, 100.21, 571.10, 1139.58, 119.27, 250.00, 116.04, 571.10
+  ),
+  vtn_maximo = c(
+    1441.14, 1479.10, 886.70, 1079.63, 1441.14, 1479.10, 981.39, 929.12, 212.83, 1441.14
+  ),
+  origem = "Canavieira, Floriano, Guadalupe, Itaueira, Jerumenha, Landri Sales, Marcos Parente,
+Nazaré do Piauí, Porto Alegre do Piauí, São Francisco do Piauí, São José do Peixe e São
+Miguel do Fidalgo"
+)
+
+
+MRT2406_2024 <- data.frame(
+  mrt = "MERCADO REGIONAL DE TERRAS VALE DO CANINDÉ (MRT-2406)",
+  tipologia_de_uso = c(
+    "Geral",
+    "Exploração Mista",
+    "Vetetacão Nativa",
+    "Exploração Mista - Diversificada",
+    "Vetetacão Nativa - Caatinga"
+  ),
+  nivel = c(0, 1, 1, 2, 2),
+  vti_media = c(
+    704.11, 697.70, 713.62, 697.70, 713.62
+  ),
+  vti_minimo = c(
+    268.00, 272.50, 268.00, 272.50, 268.00
+  ),
+  vti_maximo = c(
+    1583.33, 1373.63, 1583.33, 1373.63, 1583.33
+  ),
+  vtn_media = c(
+    583.38, 601.13, 557.58, 601.13, 557.58
+  ),
+  vtn_minimo = c(
+    251.52, 255.00, 251.52, 255.00, 251.52
+  ),
+  vtn_maximo = c(
+    1373.63, 1373.63, 1290.00, 1373.63, 1290.00
+  ),
+  origem = "Bela Vista do Piauí, Cajazeiras do Piauí, Campinas do Piauí, Campo Alegre do Fidalgo,
+Capitão Gervásio Oliveira, Colônia do Piauí, Conceição do Canindé, Floresta do Piauí,
+Isaías Coelho, João Costa, Oeiras, Paes Landim, Paquetá, Pedro Laurentino, Nova
+Santa Rita, Ribeira do Piauí, Santa Cruz do Piauí, Santa Rosa do Piauí, Santo Inácio do
+Piauí, São Francisco de Assis do Piauí, São João da Varjota, São João do Piauí, Simplício
+Mendes, Socorro do Piauí, Tanque do Piauí e Wall Ferraz"
+)
+
+MRT2407_2024 <- data.frame(
+  mrt = "MERCADO REGIONAL DE TERRAS SUDESTE PIAUIENSE (MRT-2407)",
+  tipologia_de_uso = c(
+    "Geral",
+    "Exploração Mista",
+    "Pecuária",
+    "Vetetacão Nativa",
+    "Agrícola Caju",
+    "Agrícola - Diversificada",
+    "Exploração Mista - Diversificada",
+    "Pecuária - Bovino - Pastagem Nativa",
+    "Pecuária - Diversos",
+    "Vetetacão Nativa - Caatinga"
+  ),
+  nivel = c(0, 1, 1, 1, 2, 2, 2, 2, 2, 2),
+  vti_media = c(
+    1224.43, 1351.47, 1512.12, 896.66, 1221.20, 1485.54, 1904.18, 1063.09,
+    1812.63, 896.66
+  ),
+  vti_minimo = c(
+    148.93, 295.45, 363.20, 148.93, 500.00, 687.50, 1300.00, 568.18, 363.20,
+    148.93
+  ),
+  vti_maximo = c(
+    3200.00, 2421.05, 3200.00, 1860.55, 1666.67, 2769.13, 2828.41, 1545.79,
+    3200.00, 1860.55
+  ),
+  vtn_media = c(
+    998.63, 1040.13, 1185.52, 771.31, 1121.20, 1485.54, 1398.14, 998.90,
+    1425.30, 771.31
+  ),
+  vtn_minimo = c(
+    135.93, 277.60, 250.65, 135.93, 500.00, 687.50, 297.88, 568.18, 363.20,
+    135.93
+  ),
+  vtn_maximo = c(
+    2623.21, 2421.05, 2488.20, 1627.90, 1666.67, 1234.38, 2421.05, 1545.79,
+    2488.20, 1627.90
+  ),
+  origem = "Acauã, Alagoinha do Piauí, Alegrete do Piauí, Aroeiras do Itaim, Belém do Piauí, Betânia
+do Piauí, Bocaina, Caldeirão Grande do Piauí, Campo Grande do Piauí, Caridade do
+Piauí, Curral Novo do Piauí, Dom Expedito Lopes, Francisco Macedo, Francisco Santos,
+Fronteiras, Geminiano, Ipiranga do Piauí, Itainópolis, Jacobina do Piauí, Jaicós, Lagoa
+do Barro do Piauí, Marcolândia, Massapê do Piauí, Monsenhor Hipólito, Padre Marcos,
+Patos do Piauí, Paulistana, Picos, Pio IX, Queimada Nova, Santana do Piauí, Santo
+Antônio de Lisboa, São João da Canabrava, São José do Piauí, São Julião, São Luis do
+Piauí, Simões, Sussuapara, Vera Mendes e Vila Nova do Piauí"
+)
+
+MRT2408_2024 <- data.frame(
+  mrt = "MERCADO REGIONAL DE TERRAS VALE DO MARATÕA (MRT-2408)",
+  tipologia_de_uso = c(
+    "Geral",
+    "Exploração Mista",
+    "Pecuária",
+    "Vetetacão Nativa",
+    "Exploração Mista - Diversificada",
+    "Pecuária - Diversos",
+    "Vetetacão Nativa - Cerrado"
+  ),
+  nivel = c(0, 1, 1, 1, 2, 2, 2),
+  vti_media = c(
+    922.64, 940.23, 837.99, 856.71, 940.23, 837.99, 903.62
+  ),
+  vti_minimo = c(
+    238.09, 238.09, 516.12, 387.58, 238.09, 516.12, 498.79
+  ),
+  vti_maximo = c(
+    1988.41, 1988.41, 1322.31, 1699.63, 1988.41, 1322.31, 1699.63
+  ),
+  vtn_media = c(
+    823.68, 794.53, 837.99, 856.71, 794.53, 837.99, 903.62
+  ),
+  vtn_minimo = c(
+    238.09, 238.09, 516.12, 387.58, 238.09, 516.12, 498.79
+  ),
+  vtn_maximo = c(
+    1699.63, 1416.40, 1322.31, 1699.63, 1416.40, 1322.31, 1699.63
+  ),
+  origem = "Barras, Batalha, Boa Hora, Brasileira, Cabeceiras do Piauí, Domingos Mourão,
+Esperantina, Lagoa de São Francisco, Milton Brandão, Morro do Chapéu do Piauí, Pedro
+II, Piracuruca, Piripiri, São João da Fronteira e São José do Divino"
+)
+
+
+MRT2409_2024 <- data.frame(
+  mrt = "MERCADO REGIONAL DE TERRAS MÉDIO PARNAÍBA (MRT-2409)",
+  tipologia_de_uso = c(
+    "Geral",
+    "Agrícola",
+    "Exploração Mista",
+    "Pecuária",
+    "Agrícola - Diversificada",
+    "Exploração Mista - Agrícola + Pecuária",
+    "Exploração Mista - Diversificada",
+    "Pecuária - Bovino - Pastagem Formada"
+  ),
+  nivel = c(0, 1, 1, 1, 2, 2, 2, 2),
+  vti_media = c(
+    1183.61, 4366.61, 1228.83, 789.15, 4366.61, 1287.71, 693.58, 789.15
+  ),
+  vti_minimo = c(
+    193.89, 2090.24, 305.71, 348.35, 2090.24, 499.98, 305.71, 348.35
+  ),
+  vti_maximo = c(
+    5566.25, 5566.25, 3144.34, 1594.59, 5566.25, 2000.01, 1351.35, 1594.59
+  ),
+  vtn_media = c(
+    949.79, 4133.04, 1006.64, 544.86, 4133.04, 1287.71, 648.73, 544.86
+  ),
+  vtn_minimo = c(
+    193.89, 1895.05, 204.38, 200.15, 1895.05, 499.98, 204.38, 200.15
+  ),
+  vtn_maximo = c(
+    5399.72, 5399.72, 2321.17, 1104.04, 5399.72, 2000.01, 1270.27, 1104.04
+  ),
+  origem = "Palmeirais, São Pedro do Piauí, Miguel Leão, Agricolândia, Olho D’água do Piauí,
+Lagoinha do Piauí, Água Branca, São Gonçalo do Piauí, Hugo Napoleão, Santo Antônio
+dos Milagres, Jardim do Mulato, Angical do Piauí, Amarante, Regeneração,
+Francinópolis, Barra D’alcantara, Várzea Grande, Arraial e Francisco Ayres"
+)
+
+MRT2410_2024 <- data.frame(
+  mrt = "MERCADO REGIONAL DE TERRAS VALE DA CARNAÚBA (MRT-2410)",
+  tipologia_de_uso = c(
+    "Geral",
+    "Agrícola",
+    "Exploração Mista",
+    "Pecuária",
+    "Vegetação Nativa",
+    "Agrícola - Diversificada",
+    "Exploração Mista - Agrícola + Pecuária",
+    "Exploração Mista - Diversificada",
+    "Pecuária - Bovino - Pastagem Formada",
+    "Pecuária - Bovino - Pastagem Nativa",
+    "Vetetacão Nativa - Caatinga",
+    "Vetetacão Nativa - Cerrado",
+    "Pecuária - Diversos"
+  ),
+  nivel = c(0, 1, 1, 1, 1, 2, 2, 2, 2, 2, 2, 2, 2),
+  vti_media = c(
+    908.79, 3607.14, 692.67, 1027.24, 1129.42, 3607.14, 1041.28, 667.54,
+    646.40, 743.40, 1043.27, 1215.67, 1088.30
+  ),
+  vti_minimo = c(
+    142.55, 880.95, 195.72, 142.55, 233.80, 880.95, 310.84, 195.72, 499.43,
+    232.68, 233.80, 646.72, 200.12
+  ),
+  vti_maximo = c(
+    6333.33, 6333.33, 1675.14, 2800.00, 2500.00, 6333.33, 2515.72, 1276.20,
+    781.57, 1175.87, 2500.00, 2000.00, 2599.21
+  ),
+  vtn_media = c(
+    784.24, 1496.68, 598.51, 837.25, 1129.42, 1496.68, 1041.28, 617.15, 579.02,
+    592.15, 1043.27, 1215.67, 958.36
+  ),
+  vtn_minimo = c(
+    101.29, 880.95, 185.00, 101.29, 233.80, 880.95, 310.84, 195.72, 329.77,
+    101.29, 233.80, 646.72, 200.12
+  ),
+  vtn_maximo = c(
+    2510.74, 2112.40, 1245.86, 2510.74, 2500.00, 2112.40, 2270.87, 1000.00, 781.57,
+    1175.87, 2500.00, 2000.00, 1954.01
+  ),
+  origem = "Alto Longá, Altos, Aroazes, Assunção do Piauí, Barro Duro, Beneditinos, Boqueirão do
+Piauí, Buriti dos Montes, Campo Maior, Capitão de Campos, Castelo do Piauí, Cocal de
+Telha, Coivaras, Elesbão Veloso, Inhuma, Jatobá do Piauí, Juazeiro do Piauí, Lagoa do
+Sítio, Nossa Senhora de Nazaré, Novo Oriente do Piauí, Novo Santo Antônio, Passagem
+Franca do Piauí, Pau D'Arco do Piauí, Pimenteiras, Prata do Piauí, Santa Cruz dos
+Milagres, São Félix do Piauí, São João da Serra, São Miguel da Baixa Grande, São Miguel
+do Tapuio, Sigefredo Pacheco e Valença do Piauí"
+)
+
+MRT2411_2024 <- data.frame(
+  mrt = "MERCADO REGIONAL DE TERRAS TERESINA (MRT-2411)",
+  tipologia_de_uso = c(
+    "Geral",
+    "Agrícola",
+    "Exploração Mista",
+    "Pecuária",
+    "Agrícola - Cana",
+    "Exploração Mista - Diversificada",
+    "Pecuária - Diversos"
+  ),
+  nivel = c(0, 1, 1, 1, 2, 2, 2),
+  vti_media = c(
+    1802.98, 3015.27, 1583.75, 1487.30, 3015.27, 1583.75, 1487.30
+  ),
+  vti_minimo = c(
+    525.69, 1029.76, 525.69, 635.80, 1029.76, 525.69, 635.80
+  ),
+  vti_maximo = c(
+    4618.56, 4618.56, 3409.60, 2493.37, 4618.56, 3409.60, 2493.37
+  ),
+  vtn_media = c(
+    1706.31, 2733.15, 1521.60, 1302.80, 2733.15, 1521.60, 1302.80
+  ),
+  vtn_minimo = c(
+    525.69, 1029.76, 525.69, 635.80, 1029.76, 525.69, 635.80
+  ),
+  vtn_maximo = c(
+    4285.50, 4285.50, 3409.60, 2493.37, 4285.50, 3409.60, 2493.37
+  ),
+  origem = "Teresina, Nazária, Curralinhos, Monsenhor Gil, Lagoa do Piauí, Demerval Lobão, José
+de Freitas, Lagoa Alegre e União."
+)
+
+MRT2412_2024 <- data.frame(
+  mrt = "MERCADO REGIONAL DE TERRAS PLANÍCIE LITORÂNEA (MRT-2412)",
+  tipologia_de_uso = c(
+    "Geral",
+    "Agrícola",
+    "Exploração Mista",
+    "Pecuária",
+    "Vetetacão Nativa",
+    "Agrícola - Grãos Diversos",
+    "Exploração Mista - Diversificada",
+    "Pecuária - Caprino/Ovino - Pastagem Formada",
+    "Vetetacão Nativa - Caatinga",
+    "Vetetacão Nativa - Cerrado"
+  ),
+  nivel = c(0, 1, 1, 1, 1, 2, 2, 2, 2, 2),
+  vti_media = c(
+    1630.10, 2423.18, 914.89, 2183.94, 1354.97, 2423.18, 914.89, 2183.94,
+    1203.70, 1657.50
+  ),
+  vti_minimo = c(
+    472.11, 2294.45, 630.84, 808.92, 472.11, 2294.45, 630.84, 808.92, 472.11,
+    1135.56
+  ),
+  vti_maximo = c(
+    3190.50, 2551.66, 1162.02, 3190.50, 1998.77, 2551.66, 1162.02, 3190.50,
+    1889.40, 1998.77
+  ),
+  vtn_media = c(
+    1485.80, 2316.81, 781.22, 1355.34, 1350.57, 2316.81, 781.22, 1355.34,
+    1197.11, 1657.50
+  ),
+  vtn_minimo = c(
+    257.06, 2211.41, 586.09, 684.09, 472.11, 2211.41, 586.09, 684.09, 472.11,
+    1135.56
+  ),
+  vtn_maximo = c(
+    3190.50, 2496.45, 1007.68, 1786.68, 1998.77, 2496.45, 1007.68, 1786.68,
+    1889.40, 1998.77
+  ),
+  origem = "Bom Princípio do Piauí, Buriti dos Lopes, Cajueiro da Praia, Cocal, Cocal dos Alves, Ilha
+Grande, Luís Correia e Parnaíba"
+)
+
+
+PI2023 <- rbind(
+  MRT2412_2024,
+  MRT2411_2024,
+  MRT2410_2024,
+  MRT2409_2024,
+  MRT2408_2024,
+  MRT2407_2024,
+  MRT2406_2024,
+  MRT2405_2024,
+  MRT2404_2024,
+  MRT2403_2024,
+  MRT2402_2024,
+  MRT2401_2024
+)
+
+
 ### Consolidação dos dados ####
 PI2022 <- rbind(
   MRT1_2022,
@@ -355,7 +859,8 @@ PI2020 <- rbind(
 )
 PI2020$ano <- 2020
 PI2022$ano <- 2022
-PITEMPORAL <- rbind(PI2020, PI2022)
+PI2023$ano <- 2023
+PITEMPORAL <- rbind(PI2020, PI2022, PI2023)
 PITEMPORAL$estado <- 22
 PITEMPORAL$regiao <- "nordeste"
 
