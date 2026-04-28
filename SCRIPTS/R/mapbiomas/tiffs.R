77bb83b98e6b07e2db50d10795884e41a5810638
shiro <jodomingues@usp.br>
2026-04-28 13:52:11 -0300

    ookk

--- SCRIPTS/R/mapbiomas/tiffs.R
+++ SCRIPTS/R/mapbiomas/tiffs.R
@@ -7,8 +7,7 @@ library(geobr)
 library(ipeadatar)
 library(janitor)
 library(terra)
-
-a <- st_read("C:\\Users\\jodom\\Downloads\\parcelas_financiamento_201224.gpkg-Replaced BY NEW DRIVE")
+library(exactextractr)
 
 r <- rast("C:\\Users\\jodom\\Downloads\\2015_coverage.tif")
 
@@ -17,11 +16,7 @@ df_analise <- st_read(
   layer = "df_classificado",
   quiet = TRUE   # ou o nome de camada que você usou no st_write
 )
-df_analise <- st_read(
-  dsn   = "C:/Users/jodom/OneDrive/Área de Trabalho/df_classificado.gpkg",
-  layer = "df_classificado",
-  quiet = TRUE   # ou o nome de camada que você usou no st_write
-)
+
 
 dados <- read.table("C:\\Users\\jodom\\OneDrive\\Área de Trabalho\\dados_ic\\mapbiomas\\MAPBIOMAS_BRAZIL-COVERAGE_STATISTICS-COL.10.1-MUNICIPALITIES_STATES_BIOMES.txt", 
                     sep = "\t", 
@@ -31,7 +26,30 @@ dados <- read.table("C:\\Users\\jodom\\OneDrive\\Área de Trabalho\\dados_ic\\ma
 
 
 
-df_no_geom <- st_drop_geometry(df_analise)
-df_2015 <- df_no_geom[df_no_geom$ano == 2015, ]
-df_2015 <- df_analise [df_analise $ano == 2015, ]
+
+df_2015 <- df_analise [df_analise$ano == 2015, ]
 df_mt <- df_analise [df_analise $UF == "MT", ]
+
+library(sf)
+library(terra)
+
+# 1. Criar a grade (como você já fez)
+extensao <- st_as_sfc(st_bbox(r))
+grid <- st_make_grid(extensao, cellsize = 10000, square = TRUE)
+grid_sf <- st_sf(geom = grid)
+
+# --- O PULO DO GATO ---
+
+# 2. Garantir que o seu outro data frame (regiao) tenha o mesmo CRS da grade
+# Supondo que seu df com a geometria se chame 'df_regiao'
+df_regiao <- st_transform(df_2015, st_crs(grid_sf))
+
+# 3. Filtrar a grade: Manter apenas os quadrados que interceptam a sua região
+# Isso remove todos os quadrados da grade que estão fora da área de interesse
+grid_filtrada <- grid_sf[st_intersects(grid_sf, df_regiao, sparse = FALSE), ]
+
+# 4. Converter para o formato 'terra'
+grid_vect <- vect(grid_filtrada)
+
+# 5. Extrair os valores apenas dessa área recortada
+v_counts <- extract(r, grid_vect, table)
