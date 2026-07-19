191aadfcc64e7d1289a6fa655eb321acc6d054d8
shirodragon <jodomingues@usp.br>
2026-07-19 15:34:34 -0300

    adicao das unidades de conservacao a ramt

---
+++ SCRIPTS/R/unidades_de_conservacao_geobr/juncao_ucs_ramt.R
@@ -0,0 +1,20 @@
+library(sf)
+library(tidyverse)
+
+caminho_dados <- file.path(getwd(), "data")
+arquivo_saida <- file.path(caminho_dados, "ramt_commodites_uc.gpkg")
+
+ramt <- st_read(file.path(caminho_dados, "ramt_commodites.gpkg"))
+ucs_cruzadas <- read_csv(file.path(caminho_dados, "ramt_com_uc.csv"))
+
+uc_por_municipio <- ucs_cruzadas %>%
+  select(code_mn, starts_with("UC_geobr")) |> mutate(code_mn = as.character(code_mn))
+
+ramt_ucs <- left_join(ramt, uc_por_municipio, by = c("code_muni" = "code_mn"))
+
+st_write(
+  obj = ramt_ucs,
+  dsn = arquivo_saida,
+  append = FALSE,
+  delete_layer = TRUE
+)
