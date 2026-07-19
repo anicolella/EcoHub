09e356befa6da1fb9d3d48cb4dff5f5588b7182b
shirodragon <jodomingues@usp.br>
2026-07-19 19:26:55 -0300

    indicadores sociais, continuacao do scrpit

--- SCRIPTS/R/IPEADATA/indicadores_sociais.R
+++ SCRIPTS/R/IPEADATA/indicadores_sociais.R
@@ -5,15 +5,10 @@ library(tidyverse)
 caminho_dados <- file.path(getwd(), "data")
 arquivo_saida <- file.path(caminho_dados, "ramt_commodites_uc.gpkg")
 
-b <- available_territories()
-a <- available_series()
-
-idh_muni <- b %>%
-  mutate(
-    ano = year(as.Date(date)),  # Se date for texto "YYYY-MM-DD"
-    # ou
-    # year = year(ymd(date))    # Alternativa com lubridate
-    # year = substr(date, 1, 4) # Se quiser manter como texto
-    idh = value,
-    code_muni = tcode
-  ) %>% select(code, idh_educacao, ano)
+# Rendimento médio real.trabalho principal.efetivo
+rend_med_trabalho <- ipeadata("PNADCT_RREPUF")
+ 
+rend_trabalho <- rend_med_trabalho  |>
+  mutate(ano = year(as.Date(date))) |> 
+  group_by(ano, tcode) |> 
+  summarise(ipeadata_rend_med_trabalho = mean(value, na.rm = TRUE))
